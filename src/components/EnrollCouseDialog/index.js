13685ba19426b01b7903899779e396255a3fd773
usmanashrafusman <usman.ashraf033@gmail.com>
2022-05-14 14:37:58 +0500

    form

--- src/components/EnrollCouseDialog/index.js
+++ src/components/EnrollCouseDialog/index.js
@@ -6,6 +6,7 @@ import DialogContent from '@mui/material/DialogContent';
 import SelectBox from '../forms/SelectBox';
 import DialogTitle from '@mui/material/DialogTitle';
 import Input from '../forms/Input';
+import { Grid } from '@mui/material';
 
 const cityOptions = [{
   value: "karachi",
@@ -23,14 +24,35 @@ const EnrollCouseDialog = ({ isOpen, handleClose }) => {
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth
       >
         <DialogTitle id="alert-dialog-title">
           Course Enroll Form
         </DialogTitle>
         <DialogContent>
-          <SelectBox labelTag="City" placeholder="Select City" name="city" options={cityOptions} />
-          <SelectBox labelTag="Course" placeholder="Select Course" name="courseTitle" options={cityOptions} />
-          <Input labelTag="Your Name" placeholder="Your Name" name="name"/>
+          <Grid container spacing={4}>
+            <Grid item md={4}>
+              <SelectBox labelTag="City" placeholder="Select City" name="city" options={cityOptions} />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="Your Name" placeholder="Your Name" name="fName" />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="Father Name" placeholder="Father Name" name="lName" />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="Email" type="email" placeholder="Email" name="email" />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="Phone" type="number" placeholder="Phone" name="phone" />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="CNIC" type="number" placeholder="CNIC" name="CNIC" />
+            </Grid>
+            <Grid item md={4}>
+              <Input labelTag="Father CNIC" type="number" placeholder="Father CNI" name="fCNIC" />
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
